af09bf7c5b51b723feb14e6e522d27effc3cae08
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-07 12:56:00 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -8,9 +8,16 @@ class TestSoundex(unittest.TestCase):
 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
-
-    def test_single_character_uppercase(self):
         self.assertEqual(generate_soundex("Z"), "Z000")
+
+    def test_case_insensitivity(self):
+        self.assertEqual(generate_soundex("ROBERT"), generate_soundex("robert"))
+
+    def test_vowels(self):
+        self.assertEqual(generate_soundex("Aeiou"), "A000")
+
+    def test_consonants(self):
+        self.assertEqual(generate_soundex("Bcdghjklmnpqrstvwxyz"), "B232")
 
 if __name__ == '__main__':
     unittest.main()
