cba9a1e10890f1a463d0260ae24a29dbb59655ca
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-07 12:33:11 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -2,28 +2,16 @@ def generate_soundex(name):
     if not name:
         return ""
     
-    first_letter = name[0].upper()
-    encoded = encode_remaining(name[1:].upper())
-    return pad_with_zeros(first_letter + encoded)
-
-def encode_remaining(remaining):
-    codes = [get_code(char) for char in remaining]
-    return ''.join(filter_codes(codes))
-
-def get_code(char):
+    soundex = name[0].upper()
     mapping = {'BFPV': '1', 'CGJKQSXZ': '2', 'DT': '3', 'L': '4', 'MN': '5', 'R': '6'}
-    return next((code for key, code in mapping.items() if char in key), '0')
-
-def filter_codes(codes):
-    result = []
     prev_code = '0'
-    for code in codes:
+    
+    for char in name[1:].upper():
+        code = next((v for k, v in mapping.items() if char in k), '0')
         if code != '0' and code != prev_code:
-            result.append(code)
+            soundex += code
             prev_code = code
-        if len(result) == 3:
+        if len(soundex) == 4:
             break
-    return result
-
-def pad_with_zeros(soundex):
+    
     return soundex.ljust(4, '0')
