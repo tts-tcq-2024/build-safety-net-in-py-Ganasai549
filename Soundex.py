1ef81a85c4d0ba0afeede6241fb3f82cd6ee63c9
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-07 12:36:06 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -2,16 +2,15 @@ def generate_soundex(name):
     if not name:
         return ""
     
+    codes = "01230120022455012623010202"
     soundex = name[0].upper()
-    mapping = {'BFPV': '1', 'CGJKQSXZ': '2', 'DT': '3', 'L': '4', 'MN': '5', 'R': '6'}
-    prev_code = '0'
     
     for char in name[1:].upper():
-        code = next((v for k, v in mapping.items() if char in k), '0')
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
-        if len(soundex) == 4:
-            break
+        if char.isalpha():
+            code = codes[ord(char) - ord('A')]
+            if code != '0' and code != soundex[-1]:
+                soundex += code
+                if len(soundex) == 4:
+                    break
     
     return soundex.ljust(4, '0')
