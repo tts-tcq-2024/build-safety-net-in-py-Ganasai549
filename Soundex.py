d44aab7c4fc7ad679425a0772ff786d86f17367d
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-07 12:16:04 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -3,19 +3,14 @@ def generate_soundex(name):
         return ""
     
     soundex = name[0].upper()
-    mapping = {
-        'BFPV': '1', 'CGJKQSXZ': '2', 'DT': '3',
-        'L': '4', 'MN': '5', 'R': '6'
-    }
+    mapping = {'BFPV': '1', 'CGJKQSXZ': '2', 'DT': '3', 'L': '4', 'MN': '5', 'R': '6'}
     
     for char in name[1:].upper():
-        for key in mapping:
-            if char in key:
-                code = mapping[key]
-                if code != soundex[-1]:
-                    soundex += code
+        for key, value in mapping.items():
+            if char in key and value != soundex[-1]:
+                soundex += value
+                if len(soundex) == 4:
+                    return soundex
                 break
-        if len(soundex) == 4:
-            break
     
     return soundex.ljust(4, '0')
