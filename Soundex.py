42b286a2f6008e82bdb2a7c6cf85f1a4bfebee90
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-07 12:42:24 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,13 +1,6 @@
 def generate_soundex(name):
-    if not name:
-        return ""
-    
     codes = "01230120022455012623010202"
-    soundex = name[0].upper()
-    
-    for char in name[1:].upper():
-        if char.isalpha():
-            soundex += codes[ord(char) - ord('A')]
-    
-    return (soundex[:4] + "000")[:4]
+    name = (name.upper() + "A" * 4)[:4]
+    soundex = name[0] + ''.join(codes[ord(c) - 65] for c in name[1:])
+    return soundex.ljust(4, '0')[:4]
 
